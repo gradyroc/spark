43a7a6951c067ef9c15c0d97917e64708f810b63
gradyroc <13802504607@163.com>
2022-07-24 22:02:39 +0800

    wordcount

---
+++ scala-kit/src/main/scala/cn/grady/spark/wordcount/WordCountLocal.scala
@@ -0,0 +1,31 @@
+package cn.grady.spark.wordcount
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author rociss
+ * @version 1.0, on 22:38 2022/7/23.
+ */
+object WordCountLocal {
+
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+      .setAppName("wordCount")
+      .setMaster("local")
+
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("D:\\Projects\\github\\spark-kit\\scala-kit\\src\\main\\resources\\wordCountFile")
+    val words = lines.flatMap(line => line.split(" "))
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    wordCounts.foreach(wordCount => {
+      println(s"  ${wordCount._1}  appeared  ${wordCount._2} times ")
+    })
+
+
+  }
+}
