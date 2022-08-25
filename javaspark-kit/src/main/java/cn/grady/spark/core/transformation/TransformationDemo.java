06dd5c5a98ad32588548a623aabb8247aa257f9c
gradyroc <13802504607@163.com>
2022-08-26 02:05:37 +0800

    transformation groupByKey reduceByKey sortByKey join ：交集 cogroup：key交集，value做收集

--- javaspark-kit/src/main/java/cn/grady/spark/core/transformation/TransformationDemo.java
+++ javaspark-kit/src/main/java/cn/grady/spark/core/transformation/TransformationDemo.java
@@ -6,6 +6,7 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.VoidFunction;
 import scala.Tuple2;
 
@@ -31,14 +32,164 @@ public class TransformationDemo {
 //        map();
 //        filter();
 //        flatMap();
-        groupByKey();
+//        groupByKey();
 
+//        reduceBykey();
+
+//        sortByKey();
+
+        joinAndCogroup();
         close();
+    }
+
+    /**
+     * join 和Cogroup：打印学生成绩
+     */
+    private static void joinAndCogroup() {
+        List<Tuple2<Integer,String>> studentList = Arrays.asList(
+                new Tuple2<Integer,String>(1,"leo"),
+                new Tuple2<Integer,String>(2,"tom"),
+                new Tuple2<Integer,String>(3,"marry"),
+                new Tuple2<Integer,String>(4,"jack")
+        );
+        List<Tuple2<Integer,Integer>> scoresList = Arrays.asList(
+                new Tuple2<Integer,Integer>(1,80),
+                new Tuple2<Integer,Integer>(1,70),
+                new Tuple2<Integer,Integer>(2,90),
+                new Tuple2<Integer,Integer>(2,80),
+                new Tuple2<Integer,Integer>(3,100),
+                new Tuple2<Integer,Integer>(3,50),
+                new Tuple2<Integer,Integer>(4,60)
+        );
+        JavaPairRDD<Integer, String> students = sc.parallelizePairs(studentList);
+        JavaPairRDD<Integer, Integer> scores = sc.parallelizePairs(scoresList);
+
+        //join 算子关联两个RDD
+        /**
+         *根据key进行join，并返回JavaPairRDD
+         * 但是JavaPairRDD的第一个泛型类型，之前两个JavaPairRDD的key类型是通过key进行join的
+         * 第二个泛型，是Tuple2<v1,v2>的类型，Tuple2的两个泛型类型分别为原始RDD的value的类型
+         *
+         *join 就返回RDD的每一个元素，就是 通过key join之后的一个pair
+         * etc：(1,1)(1,2) (1,3)的一个RDD
+         * 另一个RDD：(1,4)(2,1) (2,2)
+         * join 之后的结果：(1,(1,4)) (1,(2,4))(1,(3,4)) 结果的key是两个RDD的交集
+         */
+        JavaPairRDD<Integer, Tuple2<String, Integer>> studentScores = students.join(scores);
+
+//        studentScores.foreach(new VoidFunction<Tuple2<Integer, Tuple2<String, Integer>>>() {
+//            @Override
+//            public void call(Tuple2<Integer, Tuple2<String, Integer>> studentScore) throws Exception {
+//                System.out.println("student id :"+studentScore._1);
+//                System.out.println("student name:"+studentScore._2._1);
+//                System.out.println("student score:"+studentScore._2._2);
+//                System.out.println("==================================");
+//            }
+//        });
+
+        System.out.println("==================cogroup    ==========================");
+
+        /**
+         * cogroup: 与join 不同
+         * 相当于是，一个key join 上的所有value，都给放到一个Iterable 里面去了
+         *
+         */
+
+        JavaPairRDD<Integer, Tuple2<Iterable<String>, Iterable<Integer>>> cogroup = students.cogroup(scores);
+
+        cogroup.foreach(new VoidFunction<Tuple2<Integer, Tuple2<Iterable<String>, Iterable<Integer>>>>() {
+            @Override
+            public void call(Tuple2<Integer, Tuple2<Iterable<String>, Iterable<Integer>>> integerTuple2Tuple2) throws Exception {
+                System.out.println("student id :"+integerTuple2Tuple2._1);
+                System.out.println("student name:"+integerTuple2Tuple2._2._1);
+                System.out.println("student score:"+integerTuple2Tuple2._2._2);
+                System.out.println("==================================");
+            }
+        });
+
+    }
+
+    /**
+     * sortByKey 按照学生分数排序
+     */
+    private static void sortByKey() {
+        List<Tuple2<Integer,String>> scoresList = Arrays.asList(
+                new Tuple2<Integer,String>(80,"leo"),
+                new Tuple2<Integer,String>(90,"tom"),
+                new Tuple2<Integer,String>(100,"marry"),
+                new Tuple2<Integer,String>(60,"jack")
+        );
+        JavaPairRDD<Integer,String> scores = sc.parallelizePairs(scoresList);
+        /**
+         * 根据key 排序，可以指定升序还是降序
+         * 返回的还是JavaPairRDD，其中的元素内容，跟原始的RDD一模一样
+         * 只是RDD中的顺序不同
+         */
+//        JavaPairRDD<Integer, String> sortRDD = scores.sortByKey();
+        JavaPairRDD<Integer, String> sortRDD = scores.sortByKey(false);
+
+        sortRDD.foreach(new VoidFunction<Tuple2<Integer, String>>() {
+            @Override
+            public void call(Tuple2<Integer, String> score) throws Exception {
+                System.out.println(score._1+":"+score._2);
+            }
+        });
+    }
+
+    /**
+     * reduceByKey:统计班级的总分
+     */
+    private static void reduceBykey() {
+        List<Tuple2<String,Integer>> scoresList = Arrays.asList(
+                new Tuple2<String,Integer>("class1",80),
+                new Tuple2<String,Integer>("class2",90),
+                new Tuple2<String,Integer>("class1",100),
+                new Tuple2<String,Integer>("class2",60)
+        );
+
+        JavaPairRDD<String,Integer> scores = sc.parallelizePairs(scoresList);
+
+        /**
+         * Function2,3个泛型参数，
+         * 第一个和第二个泛型类型，代表了原始RDD中的元素value的类型
+         * 因此对每个key进行reduce，都会一次将第一个，第二个value传入，将值再与第三个value 传入
+         *
+         * 因此此处，会自动定义两个泛型类型，代表call()方法的两个传入参数的类型
+         * 第三个泛型类型，代表了每次reduceByKey操作返回值的类型，默认也是与原始RDD的value类型相同的
+         *返回的RDD还是JavaPairRDD<key,value>
+         */
+
+        JavaPairRDD<String, Integer> totalScores = scores.reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+            /**
+             * 对每个key，都会将其value，依次传入call方法
+             * 从而聚合出每个key对应的一个value
+             * 然后，每个key对应的一个value，组合成一个Tuple2,作为新的RDD元素
+             * @param v1
+             * @param v2
+             * @return
+             * @throws Exception
+             */
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+
+        totalScores.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+            @Override
+            public void call(Tuple2<String, Integer> tuple2) throws Exception {
+                System.out.println(tuple2._1+":"+tuple2._2);
+            }
+        });
+
     }
 
 
     /**
      * groupByKey : 每个班级的成绩进行分组
+     *
+     *
      */
 
     public static void groupByKey(){
--- scala-kit/src/main/scala/cn/grady/spark/core/transformation/TransformationOperation.scala
+++ scala-kit/src/main/scala/cn/grady/spark/core/transformation/TransformationOperation.scala
@@ -14,28 +14,96 @@ object TransformationOperation {
   val sc = new SparkContext(conf)
 
 
-
   def main(args: Array[String]): Unit = {
 
-//    map()
+    //    map()
 
-//    filter()
+    //    filter()
 
-//    flatMap()
+    //    flatMap()
 
-    groupByKey()
+    //    groupByKey()
+    //    reduceByKey()
+    //    sortByKey()
+
+    joinAndCogroup()
   }
 
+  def joinAndCogroup(): Unit = {
+    val studentList = Array(
+      Tuple2(1, "leo")
+      , Tuple2(2, "tom")
+      , Tuple2(3, "marry")
+      , Tuple2(4, "jack"))
+    val scoreList = Array(
+      Tuple2(1, 80),
+      Tuple2(1, 70),
+      Tuple2(2, 90),
+      Tuple2(2, 100),
+      Tuple2(3, 100),
+      Tuple2(3, 60),
+      Tuple2(4, 60))
+
+    val students = sc.parallelize(studentList)
+    val scores = sc.parallelize(scoreList)
+
+    val studentScores = students.join(scores)
+//    studentScores.foreach(studentScore => {
+//      println("student id :" + studentScore._1)
+//      println("student name:" + studentScore._2._1)
+//      println("student score:" + studentScore._2._2)
+//      println("=========================")
+//    })
+
+    val studentScoreCoGroup = students.cogroup(scores)
+    studentScoreCoGroup.foreach(studentScore => {
+      println("student id :" + studentScore._1)
+      println("student name:" + studentScore._2._1)
+      println("student score:" + studentScore._2._2)
+      println("=========================")
+    })
+
+
+  }
+
+  def sortByKey(): Unit = {
+    val scoreList = Array(Tuple2(65, "leo")
+      , Tuple2(90, "tom")
+      , Tuple2(80, "marry")
+      , Tuple2(70, "jack"))
+    val scoresRDD = sc.parallelize(scoreList)
+    //    val sortRDD = scoresRDD.sortByKey()
+    val sortRDD = scoresRDD.sortByKey(false)
+    sortRDD.foreach(score => {
+      println(score._1 + ":", score._2)
+    })
+  }
+
+
+  def reduceByKey(): Unit = {
+    val scoreList = Array(Tuple2("class1", 80),
+      Tuple2("class2", 90),
+      Tuple2("class1", 100),
+      Tuple2("class2", 60))
+    val scores = sc.parallelize(scoreList)
+    val totalScores = scores.reduceByKey(_ + _)
+    totalScores.foreach(classScore => {
+      println(classScore._1 + ":" + classScore._2)
+    })
+
+  }
+
+
   def groupByKey(): Unit = {
-   val scoreList = Array(Tuple2("class1",80),
-     Tuple2("class2",90),
-     Tuple2("class1",100),
-     Tuple2("class2",60))
+    val scoreList = Array(Tuple2("class1", 80),
+      Tuple2("class2", 90),
+      Tuple2("class1", 100),
+      Tuple2("class2", 60))
     val scores = sc.parallelize(scoreList)
     val groupedScores = scores.groupByKey()
-    groupedScores.foreach(group=>{
+    groupedScores.foreach(group => {
       println(group._1)
-      group._2.foreach(score=>{
+      group._2.foreach(score => {
         println(score)
       })
       println("=================")
@@ -44,30 +112,30 @@ object TransformationOperation {
 
   }
 
-  def flatMap(): Unit ={
-    val lineArray = Array("hello you","hello me","hello world")
+  def flatMap(): Unit = {
+    val lineArray = Array("hello you", "hello me", "hello world")
     val lines = sc.parallelize(lineArray)
-    val words = lines.flatMap(s=>s.split(" "))
+    val words = lines.flatMap(s => s.split(" "))
     words.foreach(println)
 
   }
 
-  def filter(): Unit ={
-    val numbers = Array(1,2,3,4,5,6,7,8,9)
+  def filter(): Unit = {
+    val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
     val numberRDD = sc.parallelize(numbers)
 
-    val evenRDD =  numberRDD.filter(num=> num%2==0)
+    val evenRDD = numberRDD.filter(num => num % 2 == 0)
     evenRDD.foreach(println)
 
   }
 
-  def  map(): Unit ={
-    val numbers = Array(1,2,3,4,5)
+  def map(): Unit = {
+    val numbers = Array(1, 2, 3, 4, 5)
     val numberRDD = sc.parallelize(numbers)
 
-//    val mapRDD = numberRDD.map(_*2)
-    val mapRDD = numberRDD.map(s=> s*2)
-//    mapRDD.foreach(println)
-    mapRDD.foreach(num=> println(num))
+    //    val mapRDD = numberRDD.map(_*2)
+    val mapRDD = numberRDD.map(s => s * 2)
+    //    mapRDD.foreach(println)
+    mapRDD.foreach(num => println(num))
   }
 }
